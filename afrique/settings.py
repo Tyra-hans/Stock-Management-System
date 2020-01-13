c1b93ed72eaa95e02b95f44c82e23a680488e207
Tyra-hans <tyrahans17@gmail.com>
2020-01-13 15:05:45 +0300

    Initial commit

--- afrique/settings.py
+++ afrique/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'inventory',
-        'USER': 'moringa',
-        'PASSWORD': 'stock',
+        'USER': 'tyra',
+        'PASSWORD': 'password',
     }
 }
 
