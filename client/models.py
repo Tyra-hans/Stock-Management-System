b883b6a52b2dbe64c062271fd63453ae2b5c380e
Titus Ouko <titusouko@gmail.com>
2020-01-13 12:12:50 +0300

    setup client's app

--- client/models.py
+++ client/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from products.models import *
 
-# Create your models here.
+class Client(models.Model):
+    c_name = models.CharField(max_length=100)
+    c_phone = models.IntegerField(max_length=14)
+    date = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def save_client(self):
+        self.save()
+
+    def delete_client(self):
+        self.delete()
+
+    def __str__(self):
+        return f'{self.c_name}'
+
+    class Meta:
+        ordering = ['-date']
