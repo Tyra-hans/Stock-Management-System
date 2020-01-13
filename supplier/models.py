c25fc576cfb5b6cc7f724f218db619f8d992a7c0
Titus Ouko <titusouko@gmail.com>
2020-01-13 11:58:27 +0300

    setup supplier's app

--- supplier/models.py
+++ supplier/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from materials.models import *
 
-# Create your models here.
+class Supplier(models.Model):
+    s_name = models.CharField(max_length=100)
+    s_phone = models.CharField(max_length=14)
+    date = models.DateTimeField(auto_now_add=True)
+    materials = models.ForeignKey('Material', on_delete=models.CASCADE)
+
+    def save_supplier(self):
+        self.save()
+
+    def delete_supplier(self):
+        self.delete()
+
+    def __str__(self):
+        return f'{self.s_name}'
+
+    class Meta:
+        ordering = ['-date']
+
+     
