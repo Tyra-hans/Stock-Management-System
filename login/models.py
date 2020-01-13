b8ae98e26eac5aa6958504c9fde65724d242da6d
Titus Ouko <titusouko@gmail.com>
2020-01-13 12:29:04 +0300

    Remove Abstract user from login models

--- login/models.py
+++ login/models.py
@@ -1,11 +1,4 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-USER_TYPE_CHOICES=(
-    (1, "Admin"),
-    (2, "Accountant"),
-    (3, "Stock manager")
-)
 
-class User(AbstractUser):
-    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
